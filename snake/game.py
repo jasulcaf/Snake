1e6c2213dfb69419a66cc2b65d22f32fbbc44bc4
Jose Sulca Flores <jasulcaf@uchicago.edu>
2025-02-23 10:32:58 -0500

    Adding the logic for the snake class

--- snake/game.py
+++ snake/game.py
@@ -1,12 +1,25 @@
 from utils.list_manipulation import print_list
+from snake.snake import Snake
+from snake.constants import *
+
 
 class Game:
     def __init__(self, height=5, width=10):
         self.height = height
         self.width = width
         self.board = [[" " for _ in range(width)] for _ in range(height)]
+        self.snake = Snake()
     
     def render(self):
+        # Update the board with the current snake positioning
+        snake_pos = self.snake.body
+        for pos in snake_pos:
+            x = pos[0]
+            y = pos[1]
+            self.board[x][y] = "0"
+        snake_head = self.snake.head()
+        self.board[snake_head[0]][snake_head[1]] = "X"
+        
         # Create a padding so that user can tell what's a wall
         corner = "+"
         wall = "-"
--- snake/snake.py
+++ snake/snake.py
@@ -1,3 +1,18 @@
+from .constants import *
+
 class Snake:
-    def __init__(self):
-        print("Snake")
+    def __init__(self, init_body=[[0,0],[1,0],[2,0],[3,0]],init_direction=RIGHT):
+        # First element in the array is the tail of the snake
+        # Last element in the array is the head of the snake
+        # By default, set this to be right
+        self.body = init_body
+        self.direction = init_direction
+    
+    def update_pos(self, position):
+        self.body = self.body[1:] + [position]
+    
+    def update_direction(self, direction):
+        self.direction = direction
+    
+    def head(self):
+        return self.body[-1]
